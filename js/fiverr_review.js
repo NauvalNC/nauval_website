f9480216e57db66acb52feb95dc7d1c99e372805
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2021-01-27 17:10:50 +0700

    Update fiverr_review.js

--- js/fiverr_review.js
+++ js/fiverr_review.js
@@ -7,7 +7,6 @@ $(document).ready(function()
     {buyer:"kayabun", country:"Malaysia", msg: "Highly recommended artist. Very patient and pleasant to work with. Thanks :D", rating: 5},
     {buyer:"lottichu", country:"Singapore", msg: "Perfect seller!! got every detail correct and very professional.", rating: 5},
     {buyer:"mokimato", country:"Canada", msg: "Excellent service and amazing to work with! went over and beyond on everything I asked for in a very quick amount of time! I am extremely happy with the final outcome and definitely would recommend :)", rating: 5},
-    {buyer:"lottichu", country:"Singapore", msg: "Perfect seller!! got every detail correct and very professional.", rating: 5},
     {buyer:"charmingneku", country:"United States", msg: "Perfect! We got everything we needed :) Will hire again for future projects.", rating: 5},
     {buyer:"davonpeoples", country:"United States", msg: "Great quality service!!!", rating: 5},
     {buyer:"pontikue", country:"Netherlands", msg: "Great service. Excellent anime style models with high quality textures!", rating: 5},
@@ -15,7 +14,9 @@ $(document).ready(function()
     {buyer:"pontikue", country:"Cyprus", msg: "Beautiful 3d models with high quality texture maps! 👍", rating: 5},
     {buyer:"creative_ed", country:"Portugal", msg: "Where should I start? I have ordered many times from fiverr now and working with Nauvalnever was hands down the best experience I have ever had. Very professional, great communication and an amazing workflow and energy. I will definitely come back for more, and you should too.", rating: 5},
     {buyer:"pontikue", country:"Cyprus", msg: "Well made 3d models with good poly counts. He agreed to my requests to add more details.", rating: 5},
-    {buyer:"pontikue", country:"Cyprus", msg: "I needed an extra outfit for one of my characters, and nauvalnever delivered quickly and with the expected quality. 👍", rating: 5}
+    {buyer:"pontikue", country:"Cyprus", msg: "I needed an extra outfit for one of my characters, and nauvalnever delivered quickly and with the expected quality. 👍", rating: 5},
+    {buyer:"mokimato", country:"Canada", msg: "Always great working with Nauvalnever! speedy delivery, amazing art, 10/10!", rating: 5},
+    {buyer:"benjamincoll875", country:"Japan", msg: "Words can not even begin to describe how blown away I am by this seller. The results are beyond what I could have hoped for. The seller was on point, kept me up to date on the process and was polite and courteous every step of the way.", rating: 5}
   ];
 
   var inner = $("#fiverrReviewIndicator .carousel-inner");
