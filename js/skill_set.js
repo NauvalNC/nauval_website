2315f0256da3d662c9ae301ed5e2ec93900309d9
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2021-02-26 22:54:22 +0700

    Update skill_set.js

--- js/skill_set.js
+++ js/skill_set.js
@@ -34,6 +34,31 @@ function displaySkillSet(name, data) {
     resume.append(item);
 }
 
+function displaySkillText(name, data) {
+    var item = "";
+    item += createSkillSet(name);
+    item += "<ul>";
+    for (var x = 0; x < data.length; x++) {
+        item += "<li>" + data[x] + "</li>";
+    }
+    item += "</ul>";
+    item ++ closeSkillSet();
+    resume.append(item);
+}
+
+// Achievement
+var acc = [
+    "Sakura Science Exchange Program Japan 2018",
+    "Game Programming Mages Institute of Excellence Singapore",
+    "Gold Medalist Gat Creative Idea 2018 Binus University",
+    "Mobile Game Dev Mages Institute of Excellence Singapore",
+    "High Distinction Applied Android Programming Binus University",
+    "Game and Web Dev Flatpastel Entmt. Excellent Predicate",
+    "First Winner Techcomfest Programming Competition 2018",
+    "Third Winner Web Design COMFEST 2017 Diponegoro University",
+    "Sololearn Courses on C++, C#, Java, Swift, Js, Jquery, Php, and More"
+];
+
 // Skills
 var skill = [
     {name: "Programming", value: 84},
@@ -66,6 +91,7 @@ var exp = [
     {name: "Any Text Editors", value: 85},
 ];
 
+displaySkillText("Achievements, Certificates, and Certifications", acc);
 displaySkillSet("Skills", skill);
 displaySkillSet("Programming", prog);
 displaySkillSet("Software Development", dev);
