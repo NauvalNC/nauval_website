01c56be1d3233ba82aa4b29140c634a4ebc6931b
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2021-03-16 11:33:59 +0800

    Update fiverr_review.js

--- js/fiverr_review.js
+++ js/fiverr_review.js
@@ -19,7 +19,8 @@ $(document).ready(function()
     {buyer:"benjamincoll875", country:"Japan", msg: "Words can not even begin to describe how blown away I am by this seller. The results are beyond what I could have hoped for. The seller was on point, kept me up to date on the process and was polite and courteous every step of the way.", rating: 5},
     {buyer:"benjamincoll875", country:"Japan", msg: "I'm blown away with how well this seller was able to convert this 2d chibi into a cute low poly model. Fiver stars plus one.", rating: 5},
     {buyer:"pontikue", country:"Cyprus", msg: "Character model looks almost exactly like the concept art I provided! I'm super happy with it 😄", rating: 5},
-    {buyer:"panuchka", country:"Finland", msg: "This seller is a real deal. If you wish to have professional quality models, awesome communication and speedy delivery, look no further! Will definitely order again.", rating: 5}
+    {buyer:"panuchka", country:"Finland", msg: "This seller is a real deal. If you wish to have professional quality models, awesome communication and speedy delivery, look no further! Will definitely order again.", rating: 5},
+    {buyer:"chrisevansdev", country:"United States", msg: "Did excellent work! We were modernizing an older character and he did a great job giving it a new cartoon feel while still preserving the original style. I highly recommend!", rating: 5},
   ];
 
   var inner = $("#fiverrReviewIndicator .carousel-inner");
