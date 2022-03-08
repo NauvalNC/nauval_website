39289ce3c0ed4e0c054269a7fe6da7d976f22de1
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2022-03-08 13:21:21 +0700

    Create 3dmodels.js

---
+++ js/3dmodels.js
@@ -0,0 +1,29 @@
+$(document).ready(function()
+{
+  var models = 
+  [
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp", alt:"United States"},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp", alt:"Nani kore"},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp", alt:"Nani kore"},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp", alt:"Nani kore"},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp", alt:"Nani kore"},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp", alt:"Nani kore"}
+  ];
+  var col = 3;
+  var row = models.length / col
+
+  var inner = $("#galllery_cont");
+  var item = "";
+  var index = 0
+  
+  for (var i = 0; i < col; i++) {
+    item += "<div class='col-lg-4 col-md-12 mb-4 mb-lg-0'>"
+    for (var j = 0; j < row; j++) {
+      if (index > models.length - 1) break;
+      item += "<img class='w-100 shadow-1-strong rounded mb-4' alt='" + models[index].alt + "' src='" + models[index].src + "'/>"
+      index++
+    }
+    item += "</div>
+  }
+  inner.append(item)
+});
