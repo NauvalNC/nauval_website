c1213b3402ad0c198497338a73038917157a61f0
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2022-03-08 14:03:19 +0700

    Update models.js

--- js/models.js
+++ js/models.js
@@ -3,16 +3,15 @@ $(document).ready(function()
   const url = "https://drive.google.com/uc?export=view&id=";
   var models = 
   [
-    {src:"1KYmAO-5wMSinh7MY76n3AdI0CtzxgMaI"},
-    {src:"1GWVjWaBMKGr8trP6qhOdnHzzFk1b-5h0"},
-    {src:"1Lfj_K1E0YUp5uLWS9ZGLiL21EaaXiUb2"},
-    {src:"1ZiRCpOv8Qz8Y-ua4hWHI6z4e9tBZ7QJO"},
-    {src:"1yIi6O_oAnhAeTq-lHBGTCoWkNNgVgRWi"},
-    {src:"1fA9jn2VyqC3oFlUK0jbxsxWZb24mtObT"},
-    {src:"184TwuCV5rP_ulGCNzn8lwgQq1MejSR3J"},
-    {src:"1yIi6O_oAnhAeTq-lHBGTCoWkNNgVgRWi"},
-    {src:"1kTqZQDKuPS_xMED1o6jEFFwoB9DeGILK"},
-    {src:"1MNcAmLzECY6OAGnPWtbWdWB4NyCMdMVg"}
+    {src:"1KYmAO-5wMSinh7MY76n3AdI0CtzxgMaI"}, //goat
+    {src:"1yIi6O_oAnhAeTq-lHBGTCoWkNNgVgRWi"}, //cyberpunk
+    {src:"184TwuCV5rP_ulGCNzn8lwgQq1MejSR3J"}, //ark
+    {src:"1MNcAmLzECY6OAGnPWtbWdWB4NyCMdMVg"}, //boss
+    {src:"1GWVjWaBMKGr8trP6qhOdnHzzFk1b-5h0"}, //knight
+    {src:"1Lfj_K1E0YUp5uLWS9ZGLiL21EaaXiUb2"}, //school
+    {src:"1ZiRCpOv8Qz8Y-ua4hWHI6z4e9tBZ7QJO"}, //nana
+    {src:"1fA9jn2VyqC3oFlUK0jbxsxWZb24mtObT"}, //god
+    {src:"1kTqZQDKuPS_xMED1o6jEFFwoB9DeGILK"}, //worker
   ];
   var col = 3;
   var row = models.length / col;
