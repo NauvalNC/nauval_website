0a306541ce01def85bc06fdc225f8d517c03ec1b
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2022-03-08 13:44:44 +0700

    Update models.js

--- js/models.js
+++ js/models.js
@@ -1,7 +1,6 @@
-cobsole.log("Nani kore")
 $(document).ready(function()
 {
-  console.log("hello world")
+  console.log("hello world");
   var models = 
   [
     {src:"https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp", alt:"United States"},
@@ -12,21 +11,21 @@ $(document).ready(function()
     {src:"https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp", alt:"Nani kore"}
   ];
   var col = 3;
-  var row = models.length / col
+  var row = models.length / col;
 
   var inner = $("#galllery_cont");
   var item = "";
-  var index = 0
+  var index = 0;
   
   for (var i = 0; i < col; i++) {
-    item += "<div class='col-lg-4 col-md-12 mb-4 mb-lg-0'>"
+    item += "<div class='col-lg-4 col-md-12 mb-4 mb-lg-0'>";
     for (var j = 0; j < row; j++) {
       if (index > models.length - 1) break;
-      item += "<img class='w-100 shadow-1-strong rounded mb-4' alt='" + models[index].alt + "' src='" + models[index].src + "'/>"
-      index++
+      item += "<img class='w-100 shadow-1-strong rounded mb-4' alt='" + models[index].alt + "' src='" + models[index].src + "'/>";
+      index++;
     }
-    item += "</div>
+    item += "</div>;
   }
-  inner.append(item)
-  console.log("hello world 2")
+  inner.append(item);
+  console.log("hello world 2");
 });
